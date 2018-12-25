878bc8b82bc0b60ed792e25de5b0765672931e81
Avinash Paliwal <43282710+avinashpaliwal@users.noreply.github.com>
2018-12-25 11:38:23 -0600

    First Commit
    Adding main files. Support files to be added.

---
+++ dataloader.py
@@ -0,0 +1,381 @@
+import torch.utils.data as data
+from PIL import Image
+import os
+import os.path
+import random
+
+
+def _make_dataset(dir):
+    """
+    Creates a 2D list of all the frames in N clips containing
+    M frames each.
+
+    2D List Structure:
+    [[frame00, frame01,...frameM]  <-- clip0
+     [frame00, frame01,...frameM]  <-- clip0
+     :
+     [frame00, frame01,...frameM]] <-- clipN
+
+    Parameters
+    ----------
+        dir : string
+            root directory containing clips.
+
+    Returns
+    -------
+        list
+            2D list described above.
+    """
+
+
+    framesPath = []
+    # Find and loop over all the clips in root `dir`.
+    for index, folder in enumerate(os.listdir(dir)):
+        clipsFolderPath = os.path.join(dir, folder)
+        # Skip items which are not folders.
+        if not (os.path.isdir(clipsFolderPath)):
+            continue
+        framesPath.append([])
+        # Find and loop over all the frames inside the clip.
+        for image in sorted(os.listdir(clipsFolderPath)):
+            # Add path to list.
+            framesPath[index].append(os.path.join(clipsFolderPath, image))
+    return framesPath
+
+
+def _pil_loader(path, cropArea=None, frameFlip=0):
+    """
+    Opens image at `path` using pil and applies data augmentation.
+
+    Parameters
+    ----------
+        path : string
+            path of the image.
+        cropArea : tuple, optional
+            coordinates for cropping image. Default: None
+        frameFlip : int, optional
+            Non zero to flip image horizontally. Default: 0
+
+    Returns
+    -------
+        list
+            2D list described above.
+    """
+
+
+    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+        # Crop image if crop area specified.
+        cropped_img = img.crop(cropArea) if (cropArea != None) else img
+        # Flip image horizontally if specified.
+        flipped_img = cropped_img.transpose(Image.FLIP_LEFT_RIGHT) if frameFlip else cropped_img
+        return flipped_img.convert('RGB')
+    
+    
+class SuperSloMo(data.Dataset):
+    """
+    A dataloader for loading N samples arranged in this way:
+
+        |-- clip0
+            |-- frame00
+            |-- frame01
+            :
+            |-- frame11
+            |-- frame12
+        |-- clip1
+            |-- frame00
+            |-- frame01
+            :
+            |-- frame11
+            |-- frame12
+        :
+        :
+        |-- clipN
+            |-- frame00
+            |-- frame01
+            :
+            |-- frame11
+            |-- frame12
+
+    ...
+
+    Attributes
+    ----------
+    framesPath : list
+        List of frames' path in the dataset.
+
+    Methods
+    -------
+    __getitem__(index)
+        Returns the sample corresponding to `index` from dataset.
+    __len__()
+        Returns the size of dataset. Invoked as len(datasetObj).
+    __repr__()
+        Returns printable representation of the dataset object.
+    """
+
+
+    def __init__(self, root, transform=None, dim=(640, 360), randomCropSize=(352, 352), train=True):
+        """
+        Parameters
+        ----------
+            root : string
+                Root directory path.
+            transform : callable, optional
+                A function/transform that takes in
+                a sample and returns a transformed version.
+                E.g, ``transforms.RandomCrop`` for images.
+            dim : tuple, optional
+                Dimensions of images in dataset. Default: (640, 360)
+            randomCropSize : tuple, optional
+                Dimensions of random crop to be applied. Default: (352, 352)
+            train : boolean, optional
+                Specifies if the dataset is for training or testing/validation.
+                `True` returns samples with data augmentation like random 
+                flipping, random cropping, etc. while `False` returns the
+                samples without randomization. Default: True
+        """
+
+
+        # Populate the list with image paths for all the
+        # frame in `root`.
+        framesPath = _make_dataset(root)
+        # Raise error if no images found in root.
+        if len(framesPath) == 0:
+            raise(RuntimeError("Found 0 files in subfolders of: " + root + "\n"))
+                
+        self.randomCropSize = randomCropSize
+        self.cropX0         = dim[0] - randomCropSize[0]
+        self.cropY0         = dim[1] - randomCropSize[1]
+        self.root           = root
+        self.transform      = transform
+        self.train          = train
+
+        self.framesPath     = framesPath
+
+    def __getitem__(self, index):
+        """
+        Returns the sample corresponding to `index` from dataset.
+
+        The sample consists of two reference frames - I0 and I1 -
+        and a random frame chosen from the 7 intermediate frames
+        available between I0 and I1 along with it's relative index.
+
+        Parameters
+        ----------
+            index : int
+                Index
+
+        Returns
+        -------
+            tuple
+                (sample, returnIndex) where sample is 
+                (I0, random_intermediate_frame, I1) and returnIndex is 
+                the position of `random_intermediate_frame`. 
+                e.g.- `returnIndex` of frame next to I0 would be 0 and
+                frame before I1 would be 6.
+        """
+
+
+        sample = []
+        
+        if (self.train):
+            ### Data Augmentation ###
+            # To select random 9 frames from 12 frames in a clip
+            firstFrame = random.randint(0, 3)
+            # Apply random crop on the 9 input frames
+            cropX = random.randint(0, self.cropX0)
+            cropY = random.randint(0, self.cropY0)
+            cropArea = (cropX, cropY, cropX + self.randomCropSize[0], cropY + self.randomCropSize[1])
+            # Random reverse frame
+            #frameRange = range(firstFrame, firstFrame + 9) if (random.randint(0, 1)) else range(firstFrame + 8, firstFrame - 1, -1)
+            IFrameIndex = random.randint(firstFrame + 1, firstFrame + 7)
+            if (random.randint(0, 1)):
+                frameRange = [firstFrame, IFrameIndex, firstFrame + 8]
+                returnIndex = IFrameIndex - firstFrame - 1
+            else:
+                frameRange = [firstFrame + 8, IFrameIndex, firstFrame]
+                returnIndex = firstFrame - IFrameIndex + 7
+            # Random flip frame
+            randomFrameFlip = random.randint(0, 1)
+        else:
+            # Fixed settings to return same samples every epoch.
+            # For validation/test sets.
+            firstFrame = 0
+            cropArea = (0, 0, self.randomCropSize[0], self.randomCropSize[1])
+            IFrameIndex = ((index) % 7  + 1)
+            returnIndex = IFrameIndex - 1
+            frameRange = [0, IFrameIndex, 8]
+            randomFrameFlip = 0
+        
+        # Loop over for all frames corresponding to the `index`.
+        for frameIndex in frameRange:
+            # Open image using pil and augment the image.
+            image = _pil_loader(self.framesPath[index][frameIndex], cropArea, randomFrameFlip)
+            # Apply transformation if specified.
+            if self.transform is not None:
+                image = self.transform(image)
+            sample.append(image)
+            
+        return sample, returnIndex
+
+
+    def __len__(self):
+        """
+        Returns the size of dataset. Invoked as len(datasetObj).
+
+        Returns
+        -------
+            int
+                number of samples.
+        """
+
+
+        return len(self.framesPath)
+
+    def __repr__(self):
+        """
+        Returns printable representation of the dataset object.
+
+        Returns
+        -------
+            string
+                info.
+        """
+
+
+        fmt_str = 'Dataset ' + self.__class__.__name__ + '\n'
+        fmt_str += '    Number of datapoints: {}\n'.format(self.__len__())
+        fmt_str += '    Root Location: {}\n'.format(self.root)
+        tmp = '    Transforms (if any): '
+        fmt_str += '{0}{1}\n'.format(tmp, self.transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
+        return fmt_str
+    
+class UCI101Test(data.Dataset):
+    """
+    A dataloader for loading N samples arranged in this way:
+
+        |-- clip0
+            |-- frame00
+            |-- frame01
+            |-- frame02
+        |-- clip1
+            |-- frame00
+            |-- frame01
+            |-- frame02
+        :
+        :
+        |-- clipN
+            |-- frame00
+            |-- frame01
+            |-- frame02
+
+    ...
+
+    Attributes
+    ----------
+    framesPath : list
+        List of frames' path in the dataset.
+
+    Methods
+    -------
+    __getitem__(index)
+        Returns the sample corresponding to `index` from dataset.
+    __len__()
+        Returns the size of dataset. Invoked as len(datasetObj).
+    __repr__()
+        Returns printable representation of the dataset object.
+    """
+
+
+    def __init__(self, root, transform=None):
+        """
+        Parameters
+        ----------
+            root : string
+                Root directory path.
+            transform : callable, optional
+                A function/transform that takes in
+                a sample and returns a transformed version.
+                E.g, ``transforms.RandomCrop`` for images.
+        """
+
+
+        # Populate the list with image paths for all the
+        # frame in `root`.
+        framesPath = _make_dataset(root)
+        # Raise error if no images found in root.
+        if len(framesPath) == 0:
+            raise(RuntimeError("Found 0 files in subfolders of: " + root + "\n"))
+
+        self.root           = root
+        self.framesPath     = framesPath
+        self.transform      = transform
+
+    def __getitem__(self, index):
+        """
+        Returns the sample corresponding to `index` from dataset.
+
+        The sample consists of two reference frames - I0 and I1 -
+        and a intermediate frame between I0 and I1.
+
+        Parameters
+        ----------
+            index : int
+                Index
+
+        Returns
+        -------
+            tuple
+                (sample, returnIndex) where sample is 
+                (I0, intermediate_frame, I1) and returnIndex is 
+                the position of `intermediate_frame`.
+                The returnIndex is always 3 and is being returned
+                to maintain compatibility with the `SuperSloMo`
+                dataloader where 3 corresponds to the middle frame.
+        """
+
+
+        sample = []
+        # Loop over for all frames corresponding to the `index`.
+        for framePath in self.framesPath[index]:
+            # Open image using pil.
+            image = _pil_loader(framePath)
+            # Apply transformation if specified.
+            if self.transform is not None:
+                image = self.transform(image)
+            sample.append(image)
+        return sample, 3
+
+
+    def __len__(self):
+        """
+        Returns the size of dataset. Invoked as len(datasetObj).
+
+        Returns
+        -------
+            int
+                number of samples.
+        """
+
+
+        return len(self.framesPath)
+
+    def __repr__(self):
+        """
+        Returns printable representation of the dataset object.
+
+        Returns
+        -------
+            string
+                info.
+        """
+
+
+        fmt_str = 'Dataset ' + self.__class__.__name__ + '\n'
+        fmt_str += '    Number of datapoints: {}\n'.format(self.__len__())
+        fmt_str += '    Root Location: {}\n'.format(self.root)
+        tmp = '    Transforms (if any): '
+        fmt_str += '{0}{1}\n'.format(tmp, self.transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
+        return fmt_str
---
+++ model.py
@@ -0,0 +1,361 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+
+class down(nn.Module):
+    """
+    A class for creating neural network blocks containing layers:
+    
+    Average Pooling --> Convlution + Leaky ReLU --> Convolution + Leaky ReLU
+    
+    This is used in the UNet Class to create a UNet like NN architecture.
+
+    ...
+
+    Methods
+    -------
+    forward(x)
+        Returns output tensor after passing input `x` to the neural network
+        block.
+    """
+
+
+    def __init__(self, inChannels, outChannels, filterSize):
+        """
+        Parameters
+        ----------
+            inChannels : int
+                number of input channels for the first convolutional layer.
+            outChannels : int
+                number of output channels for the first convolutional layer.
+                This is also used as input and output channels for the
+                second convolutional layer.
+            filterSize : int
+                filter size for the convolution filter. input N would create
+                a N x N filter.
+        """
+
+
+        super(down, self).__init__()
+        # Initialize convolutional layers.
+        self.conv1 = nn.Conv2d(inChannels,  outChannels, filterSize, stride=1, padding=int((filterSize - 1) / 2))
+        self.conv2 = nn.Conv2d(outChannels, outChannels, filterSize, stride=1, padding=int((filterSize - 1) / 2))
+           
+    def forward(self, x):
+        """
+        Returns output tensor after passing input `x` to the neural network
+        block.
+
+        Parameters
+        ----------
+            x : tensor
+                input to the NN block.
+
+        Returns
+        -------
+            tensor
+                output of the NN block.
+        """
+
+
+        # Average pooling with kernel size 2 (2 x 2).
+        x = F.avg_pool2d(x, 2)
+        # Convolution + Leaky ReLU
+        x = F.leaky_relu(self.conv1(x), negative_slope = 0.1)
+        # Convolution + Leaky ReLU
+        x = F.leaky_relu(self.conv2(x), negative_slope = 0.1)
+        return x
+    
+class up(nn.Module):
+    """
+    A class for creating neural network blocks containing layers:
+    
+    Bilinear interpolation --> Convlution + Leaky ReLU --> Convolution + Leaky ReLU
+    
+    This is used in the UNet Class to create a UNet like NN architecture.
+
+    ...
+
+    Methods
+    -------
+    forward(x, skpCn)
+        Returns output tensor after passing input `x` to the neural network
+        block.
+    """
+
+
+    def __init__(self, inChannels, outChannels):
+        """
+        Parameters
+        ----------
+            inChannels : int
+                number of input channels for the first convolutional layer.
+            outChannels : int
+                number of output channels for the first convolutional layer.
+                This is also used for setting input and output channels for
+                the second convolutional layer.
+        """
+
+        
+        super(up, self).__init__()
+        # Initialize convolutional layers.
+        self.conv1 = nn.Conv2d(inChannels,  outChannels, 3, stride=1, padding=1)
+        # (2 * outChannels) is used for accomodating skip connection.
+        self.conv2 = nn.Conv2d(2 * outChannels, outChannels, 3, stride=1, padding=1)
+           
+    def forward(self, x, skpCn):
+        """
+        Returns output tensor after passing input `x` to the neural network
+        block.
+
+        Parameters
+        ----------
+            x : tensor
+                input to the NN block.
+            skpCn : tensor
+                skip connection input to the NN block.
+
+        Returns
+        -------
+            tensor
+                output of the NN block.
+        """
+
+        # Bilinear interpolation with scaling 2.
+        x = F.interpolate(x, scale_factor=2, mode='bilinear')
+        # Convolution + Leaky ReLU
+        x = F.leaky_relu(self.conv1(x), negative_slope = 0.1)
+        # Convolution + Leaky ReLU on (`x`, `skpCn`)
+        x = F.leaky_relu(self.conv2(torch.cat((x, skpCn), 1)), negative_slope = 0.1)
+        return x
+
+
+
+class UNet(nn.Module):
+    """
+    A class for creating UNet like architecture as specified by the
+    Super SloMo paper.
+    
+    ...
+
+    Methods
+    -------
+    forward(x)
+        Returns output tensor after passing input `x` to the neural network
+        block.
+    """
+
+
+    def __init__(self, inChannels, outChannels):
+        """
+        Parameters
+        ----------
+            inChannels : int
+                number of input channels for the UNet.
+            outChannels : int
+                number of output channels for the UNet.
+        """
+
+        
+        super(UNet, self).__init__()
+        # Initialize neural network blocks.
+        self.conv1 = nn.Conv2d(inChannels, 32, 7, stride=1, padding=3)
+        self.conv2 = nn.Conv2d(32, 32, 7, stride=1, padding=3)
+        self.down1 = down(32, 64, 5)
+        self.down2 = down(64, 128, 3)
+        self.down3 = down(128, 256, 3)
+        self.down4 = down(256, 512, 3)
+        self.down5 = down(512, 512, 3)
+        self.up1   = up(512, 512)
+        self.up2   = up(512, 256)
+        self.up3   = up(256, 128)
+        self.up4   = up(128, 64)
+        self.up5   = up(64, 32)
+        self.conv3 = nn.Conv2d(32, outChannels, 3, stride=1, padding=1)
+        
+    def forward(self, x):
+        """
+        Returns output tensor after passing input `x` to the neural network.
+
+        Parameters
+        ----------
+            x : tensor
+                input to the UNet.
+
+        Returns
+        -------
+            tensor
+                output of the UNet.
+        """
+
+
+        x  = F.leaky_relu(self.conv1(x), negative_slope = 0.1)
+        s1 = F.leaky_relu(self.conv2(x), negative_slope = 0.1)
+        s2 = self.down1(s1)
+        s3 = self.down2(s2)
+        s4 = self.down3(s3)
+        s5 = self.down4(s4)
+        x  = self.down5(s5)
+        x  = self.up1(x, s5)
+        x  = self.up2(x, s4)
+        x  = self.up3(x, s3)
+        x  = self.up4(x, s2)
+        x  = self.up5(x, s1)
+        x  = F.leaky_relu(self.conv3(x), negative_slope = 0.1)
+        return x
+
+
+class backWarp(nn.Module):
+    """
+    A class for creating a backwarping object.
+
+    This is used for backwarping to an image:
+
+    Given optical flow from frame I0 to I1 --> F_0_1 and frame I1, 
+    it generates I0 <-- backwarp(F_0_1, I1).
+
+    ...
+
+    Methods
+    -------
+    forward(x)
+        Returns output tensor after passing input `img` and `flow` to the backwarping
+        block.
+    """
+
+
+    def __init__(self, W, H, device):
+        """
+        Parameters
+        ----------
+            W : int
+                width of the image.
+            H : int
+                height of the image.
+            device : device
+                computation device (cpu/cuda). 
+        """
+
+
+        super(backWarp, self).__init__()
+        # create a grid
+        gridX, gridY = np.meshgrid(np.arange(W), np.arange(H))
+        self.W = W
+        self.H = H
+        self.gridX = torch.tensor(gridX, requires_grad=False, device=device)
+        self.gridY = torch.tensor(gridY, requires_grad=False, device=device)
+        
+    def forward(self, img, flow):
+        """
+        Returns output tensor after passing input `img` and `flow` to the backwarping
+        block.
+        I0  = backwarp(I1, F_0_1)
+
+        Parameters
+        ----------
+            img : tensor
+                frame I1.
+            flow : tensor
+                optical flow from I0 and I1: F_0_1.
+
+        Returns
+        -------
+            tensor
+                frame I0.
+        """
+
+
+        # Extract horizontal and vertical flows.
+        u = flow[:, 0, :, :]
+        v = flow[:, 1, :, :]
+        x = self.gridX.unsqueeze(0).expand_as(u).float() + u
+        y = self.gridY.unsqueeze(0).expand_as(v).float() + v
+        # range -1 to 1
+        x = 2*(x/self.W - 0.5)
+        y = 2*(y/self.H - 0.5)
+        # stacking X and Y
+        grid = torch.stack((x,y), dim=3)
+        # Sample pixels using bilinear interpolation.
+        imgOut = torch.nn.functional.grid_sample(img, grid)
+        return imgOut
+
+
+# Creating an array of `t` values for the 7 intermediate frames between
+# reference frames I0 and I1. 
+t = np.linspace(0.125, 0.875, 7)
+
+def getFlowCoeff (indices, device):
+    """
+    Gets flow coefficients used for calculating intermediate optical
+    flows from optical flows between I0 and I1: F_0_1 and F_1_0.
+
+    F_t_0 = C00 x F_0_1 + C01 x F_1_0
+    F_t_1 = C10 x F_0_1 + C11 x F_1_0
+
+    where,
+    C00 = -(1 - t) x t
+    C01 = t x t
+    C10 = (1 - t) x (1 - t)
+    C11 = -t x (1 - t)
+
+    Parameters
+    ----------
+        indices : tensor
+            indices corresponding to the intermediate frame positions
+            of all samples in the batch.
+        device : device
+                computation device (cpu/cuda). 
+
+    Returns
+    -------
+        tensor
+            coefficients C00, C01, C10, C11.
+    """
+
+
+    # Convert indices tensor to numpy array
+    ind = indices.detach().numpy()
+    C11 = C00 = - (1 - (t[ind])) * (t[ind])
+    C01 = (t[ind]) * (t[ind])
+    C10 = (1 - (t[ind])) * (1 - (t[ind]))
+    return torch.Tensor(C00)[None, None, None, :].permute(3, 0, 1, 2).to(device), torch.Tensor(C01)[None, None, None, :].permute(3, 0, 1, 2).to(device), torch.Tensor(C10)[None, None, None, :].permute(3, 0, 1, 2).to(device), torch.Tensor(C11)[None, None, None, :].permute(3, 0, 1, 2).to(device)
+
+def getWarpCoeff (indices, device):
+    """
+    Gets coefficients used for calculating final intermediate 
+    frame `It_gen` from backwarped images using flows F_t_0 and F_t_1.
+
+    It_gen = (C0 x V_t_0 x g_I_0_F_t_0 + C1 x V_t_1 x g_I_1_F_t_1) / (C0 x V_t_0 + C1 x V_t_1)
+
+    where,
+    C0 = 1 - t
+    C1 = t
+
+    V_t_0, V_t_1 --> visibility maps
+    g_I_0_F_t_0, g_I_1_F_t_1 --> backwarped intermediate frames
+
+    Parameters
+    ----------
+        indices : tensor
+            indices corresponding to the intermediate frame positions
+            of all samples in the batch.
+        device : device
+                computation device (cpu/cuda). 
+
+    Returns
+    -------
+        tensor
+            coefficients C0 and C1.
+    """
+
+
+    # Convert indices tensor to numpy array
+    ind = indices.detach().numpy()
+    C0 = 1 - t[ind]
+    C1 = t[ind]
+    return torch.Tensor(C0)[None, None, None, :].permute(3, 0, 1, 2).to(device), torch.Tensor(C1)[None, None, None, :].permute(3, 0, 1, 2).to(device)
