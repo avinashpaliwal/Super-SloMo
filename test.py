98411e351d4350c64c810c379947253528074afb
cch98 <changho9808@gmail.com>
2019-03-24 23:22:41 +0900

    for the bug fix: File "test.py", line 69, in make_frames     x = trans_backward(img) RuntimeError: expected type torch.cuda.FloatTensor but got torch.FloatTensor

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ import torch
 from PIL import Image
 import numpy as np
 import model
-import eval
 from torchvision import transforms
 from torch.functional import F
 
@@ -26,12 +25,60 @@ flow = model.UNet(6, 4).to(device)
 interp = model.UNet(20, 5).to(device)
 back_warp = None
 
+
+def setup_back_warp(w, h):
+    global back_warp
+    with torch.set_grad_enabled(False):
+        back_warp = model.backWarp(w, h, device).to(device)
+
 def load_models(checkpoint):
     states = torch.load(checkpoint, map_location='cpu')
     interp.load_state_dict(states['state_dictAT'])
     flow.load_state_dict(states['state_dictFC'])
 
+def interpolate_batch(frames, factor):
+    frame0 = torch.stack(frames[:-1])
+    frame1 = torch.stack(frames[1:])
 
+    i0 = frame0.to(device)
+    i1 = frame1.to(device)
+    ix = torch.cat([i0, i1], dim=1)
+
+    flow_out = flow(ix)
+    f01 = flow_out[:, :2, :, :]
+    f10 = flow_out[:, 2:, :, :]
+
+    frame_buffer = []
+    for i in range(1, factor):
+        t = i / factor
+        temp = -t * (1 - t)
+        co_eff = [temp, t * t, (1 - t) * (1 - t), temp]
+
+        ft0 = co_eff[0] * f01 + co_eff[1] * f10
+        ft1 = co_eff[2] * f01 + co_eff[3] * f10
+
+        gi0ft0 = back_warp(i0, ft0)
+        gi1ft1 = back_warp(i1, ft1)
+
+        iy = torch.cat((i0, i1, f01, f10, ft1, ft0, gi1ft1, gi0ft0), dim=1)
+        io = interp(iy)
+
+        ft0f = io[:, :2, :, :] + ft0
+        ft1f = io[:, 2:4, :, :] + ft1
+        vt0 = F.sigmoid(io[:, 4:5, :, :])
+        vt1 = 1 - vt0
+
+        gi0ft0f = back_warp(i0, ft0f)
+        gi1ft1f = back_warp(i1, ft1f)
+
+        co_eff = [1 - t, t]
+
+        ft_p = (co_eff[0] * vt0 * gi0ft0f + co_eff[1] * vt1 * gi1ft1f) / \
+               (co_eff[0] * vt0 + co_eff[1] * vt1)
+
+        frame_buffer.append(ft_p)
+
+    return frame_buffer
 
 def make_frames(I0, I1, dest, factor, output_format = 'png'):
 
@@ -47,7 +94,7 @@ def make_frames(I0, I1, dest, factor, output_format = 'png'):
     w, h = (w0 // 32) * 32, (h0 // 32) * 32
 
     #setup the back warping function in model
-    eval.setup_back_warp(w, h)
+    setup_back_warp(w, h)
 
     #image to tensor
     I0 = I0.resize((w, h), Image.ANTIALIAS)
@@ -59,7 +106,7 @@ def make_frames(I0, I1, dest, factor, output_format = 'png'):
     batch.append(I0)
     batch.append(I1)
 
-    intermediate_frames = eval.interpolate_batch(batch, factor)
+    intermediate_frames = interpolate_batch(batch, factor)
     intermediate_frames = list(zip(*intermediate_frames))
 
     intermediate_frames = intermediate_frames[0]
