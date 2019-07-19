4dd9df631e027e1551c29825deb3523418b2b69e
Steve <stg7@gmx.de>
2019-07-19 18:24:57 +0200

    change file extension

--- video_to_slomo.py
+++ video_to_slomo.py
@@ -199,7 +199,7 @@ def main():
 
                 # Save intermediate frame
                 for batchIndex in range(args.batch_size):
-                    (TP(Ft_p[batchIndex].cpu().detach())).resize(videoFrames.origDim, Image.BILINEAR).save(os.path.join(outputPath, str(frameCounter + args.sf * batchIndex) + ".jpg"))
+                    (TP(Ft_p[batchIndex].cpu().detach())).resize(videoFrames.origDim, Image.BILINEAR).save(os.path.join(outputPath, str(frameCounter + args.sf * batchIndex) + ".png"))
                 frameCounter += 1
 
             # Set counter accounting for batching of frames
