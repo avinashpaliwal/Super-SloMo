457e9263b33c378fba13734db2efcd997004af40
Steve <stg7@gmx.de>
2019-07-19 17:36:12 +0200

    remove quality scaling part

--- video_to_slomo.py
+++ video_to_slomo.py
@@ -68,14 +68,14 @@ def extract_frames(video, outDir):
 
     error = ""
     print('{} -i {} -vsync 0 -qscale:v 2 {}/%06d.jpg'.format(os.path.join(args.ffmpeg_dir, "ffmpeg"), video, outDir))
-    retn = os.system('{} -i {} -vsync 0 -qscale:v 2 {}/%06d.png'.format(os.path.join(args.ffmpeg_dir, "ffmpeg"), video, outDir))
+    retn = os.system('{} -i {} -vsync 0 {}/%06d.png'.format(os.path.join(args.ffmpeg_dir, "ffmpeg"), video, outDir))
     if retn:
         error = "Error converting file:{}. Exiting.".format(video)
     return error
 
 def create_video(dir):
     error = ""
-    print('{} -r {} -i {}/%d.jpg -qscale:v 2 {}'.format(os.path.join(args.ffmpeg_dir, "ffmpeg"), args.fps, dir, args.output))
+    print('{} -r {} -i {}/%d.jpg  {}'.format(os.path.join(args.ffmpeg_dir, "ffmpeg"), args.fps, dir, args.output))
     retn = os.system('{} -r {} -i {}/%d.png -vcodec ffvhuff {}'.format(os.path.join(args.ffmpeg_dir, "ffmpeg"), args.fps, dir, args.output))
     if retn:
         error = "Error creating output video. Exiting."
