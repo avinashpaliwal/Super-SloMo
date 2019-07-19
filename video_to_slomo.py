90a5c27ef50a38acf1220e10969712cf907268d6
Steve <stg7@gmx.de>
2019-07-19 18:30:00 +0200

    save everything as png

--- video_to_slomo.py
+++ video_to_slomo.py
@@ -67,7 +67,7 @@ def extract_frames(video, outDir):
 
 
     error = ""
-    print('{} -i {} -vsync 0 -qscale:v 2 {}/%06d.jpg'.format(os.path.join(args.ffmpeg_dir, "ffmpeg"), video, outDir))
+    print('{} -i {} -vsync 0 -qscale:v 2 {}/%06d.png'.format(os.path.join(args.ffmpeg_dir, "ffmpeg"), video, outDir))
     retn = os.system('{} -i {} -vsync 0 {}/%06d.png'.format(os.path.join(args.ffmpeg_dir, "ffmpeg"), video, outDir))
     if retn:
         error = "Error converting file:{}. Exiting.".format(video)
@@ -75,7 +75,7 @@ def extract_frames(video, outDir):
 
 def create_video(dir):
     error = ""
-    print('{} -r {} -i {}/%d.jpg  {}'.format(os.path.join(args.ffmpeg_dir, "ffmpeg"), args.fps, dir, args.output))
+    print('{} -r {} -i {}/%d.png  {}'.format(os.path.join(args.ffmpeg_dir, "ffmpeg"), args.fps, dir, args.output))
     retn = os.system('{} -r {} -i {}/%d.png -vcodec ffvhuff {}'.format(os.path.join(args.ffmpeg_dir, "ffmpeg"), args.fps, dir, args.output))
     if retn:
         error = "Error creating output video. Exiting."
@@ -168,7 +168,7 @@ def main():
 
             # Save reference frames in output folder
             for batchIndex in range(args.batch_size):
-                (TP(frame0[batchIndex].detach())).resize(videoFrames.origDim, Image.BILINEAR).save(os.path.join(outputPath, str(frameCounter + args.sf * batchIndex) + ".jpg"))
+                (TP(frame0[batchIndex].detach())).resize(videoFrames.origDim, Image.BILINEAR).save(os.path.join(outputPath, str(frameCounter + args.sf * batchIndex) + ".png"))
             frameCounter += 1
 
             # Generate intermediate frames
